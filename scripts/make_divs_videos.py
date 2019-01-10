dace33953593530cb507e10e86acbebbb5fce3e2
Smerdatus <smerdatus@gmail.com>
2019-01-09 21:25:47 -0500

    add offset

--- scripts/make_divs_videos.py
+++ scripts/make_divs_videos.py
@@ -7,18 +7,20 @@
 
 array = []
 
-i = 0
+offset = 398
 
-while i < len(array):
-    video = "./assets/krotchy/videos/{}".format(array[i]) # UPDATE THIS
+i = 0 + offset
+
+while i < len(array) + offset:
+    video = "./assets/krotchy/videos/{}".format(array[i - offset]) # UPDATE THIS
     img = "{}.png".format(video) 
     print("import video_{} from '{}';".format(i, video))
     print("import img_{} from '{}';".format(i, img))
     i += 1
 
-i = 0
+i = 0 + offset
 
-while i < len(array):
+while i < len(array) + offset:
     video = "video_{}".format(i)
     img = "img_{}".format(i) 
     print("          <div style={{{{padding: '1vh' }}}} onClick={{() => this.setState({{mode: 'video', video: {}}})}}>\n            <img src={{{}}} className={{ this.state.picSizeMode }} />\n            <img src={{overlay}} style={{{{ height: '2vh' }}}} />\n          </div>".format(video, img))
